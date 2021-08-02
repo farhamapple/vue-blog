5335ac33eeff013cf2628c17d6b4b389c5c89a8f
Farham Harvianto <lipi.farham@gmail.com>
2021-08-02 23:28:49 +0700

    master

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer">
+      <!-- -->
+      <v-list>
+        <v-list-item v-if="!guest">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portrails/men/78.jpg"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>Farham Harvianto</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+
+        <div class="pa-2" v-if="guest">
+          <v-btn block color="primary" class="mb-1">
+            <v-icon left>mdi-lock</v-icon>
+            Login
+          </v-btn>
+          <v-btn block color="success">
+            <v-icon left>mdi-account</v-icon>
+            Register
+          </v-btn>
+        </div>
+        <v-divider></v-divider>
+        <v-list-item 
+        v-for="(item, index) in menus" 
+        :key="`menu-${index}`" 
+        :to="item.route">
+          <v-list-item-icon>
+            <v-icon left>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append v-if="!guest">
+        <div class="pa-2">
+          <v-btn block color="red" dark @click="logout">
+            <v-icon left>
+              mdi-lock
+            </v-icon>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+      
+    </v-navigation-drawer>
+
+    <v-app-bar app color="success" dark>
+      <!-- -->
+      <v-app-bar-nav-icon @click.stop="drawer- !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>SanberCoe App</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <v-slide-y-transition>
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+      @SanberCode | Vue JS
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    drawer : true,
+    menus : [
+      {
+        title: 'Home',
+        icon : 'mdi-home',
+        route : '/'
+      },
+      {
+        title: 'Blogs',
+        icon : 'mdi-note',
+        route : '/blogs'
+      },
+    ],
+    guest : true
+  }),
+
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+// new Vue({
+//   el : '#app',
+//   components : { App },
+//   template : '<App />'
+// });
